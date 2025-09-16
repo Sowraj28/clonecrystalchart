a71691ab467d5af052a728c22702e706cfaa6e63
Sowraj S <sowrajsethu2004@gamil.com>
2025-09-16 20:59:39 +0530

    new redirect

--- backend-drf/stock_prediction_main/urls.py
+++ backend-drf/stock_prediction_main/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include, redirect # Add redirect
+from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
