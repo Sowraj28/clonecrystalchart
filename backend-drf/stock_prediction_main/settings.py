86482cf047db2bcdeefe7e1617d3a21bde4ebdb1
Sowraj S <sowrajsethu2004@gamil.com>
2025-09-16 21:34:47 +0530

    Update CORS_ALLOWED_ORIGINS for Netlify frontend2

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -107,7 +107,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://your-frontend.netlify.app",
+    "https://crystalchart.netlify.app",
     "https://crystal-chart.onrender.com",
 ]
 
